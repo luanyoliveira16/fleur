0f7de02565352779fbe0c00e3c588b4b6efee2cf
ChrisMaga <chris_maga@hotmail.com>
2025-09-05 23:00:43 -0300

    inclusão do arquivo de cores principais

---
+++ constants/colorApp.ts
@@ -0,0 +1,15 @@
+export const colors = {
+    white: "#fff",    
+    
+    backgroundTela: "#fffaf7", 
+
+    purple: "#792a62",
+
+    gray: {
+        800: "#252525ff",
+        500: "#525151ff",
+        300: "#888888ff",
+        100: "#d9d9d9",
+    }
+
+}
